2c505373500f05ec788919be1ebaebc589b58839
Savindu123 <an82334@gmail.com>
2024-06-14 14:33:40 +0530

    POM commit

--- src/main/java/com/salesplay/LoginPage.java
+++ src/main/java/com/salesplay/LoginPage.java
@@ -38,4 +38,3 @@ public class LoginPage {
         return driver.findElement(validationMessage).getText();
     }
 }
-
--- src/test/java/salesplay/tests/LoginTest.java
+++ src/test/java/salesplay/tests/LoginTest.java
@@ -4,10 +4,7 @@ import com.salesplay.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.apache.commons.csv.CSVFormat;
@@ -25,14 +22,14 @@ public class LoginTest {
     private LoginPage loginPage;
 
     @BeforeClass
-    public void setUp() {
-        // Set up ChromeDriver
-//        String baseUrl = "https://webpos.salesplaypos.com/sign_in";
-//        driver = WebDriverManager.chromedriver().create();
-//        System.out.println("Chrome is Starting");
-//        driver.get(baseUrl);
+    public void beforeClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void setUp() {
         driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
         loginPage = new LoginPage(driver);
         driver.manage().window().maximize();
     }
@@ -44,7 +41,6 @@ public class LoginTest {
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
         for (CSVRecord record : records) {
             loginData.add(new Object[]{record.get("email"), record.get("password"), record.get("expectedmessage")});
-
         }
         return loginData.iterator();
     }
@@ -56,17 +52,19 @@ public class LoginTest {
             loginPage.enterPassword(password);
             loginPage.clickLoginButton();
 
-            Thread.sleep(1000);
+            Thread.sleep(2000);
 
             String actualValidationMessage = loginPage.getValidationMessage();
 
             assertEquals(actualValidationMessage, expectedmessage);
 
         }
-        @AfterClass
-        public void tearDown() {
-            if (driver != null) {
-                driver.quit();
-            }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
         }
+    }
+
     }
